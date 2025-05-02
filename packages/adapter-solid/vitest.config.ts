15556c69d40f429d20167a826d207384467bb4fe
apademide <contact@apademide.ch>
2025-05-02 14:08:58 +0200

    meta: can test lib from src or dist

--- packages/adapter-solid/vitest.config.ts
+++ packages/adapter-solid/vitest.config.ts
@@ -1,14 +1,29 @@
 import path from "path";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    coverage: { provider: "v8" },
-  },
-  resolve: {
-    alias: {
-      "@auaust/pont-core": path.resolve(__dirname, "../core/src"),
-      "@auaust/pont-adapter-solid": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  // If `--mode build` is used, we want to run tests using the built
+  // version of the package. This is done by including or excluding
+  // `@auaust/pont-core` and `@auaust/pont-adapter-solid` paths in the
+  // `alias` section of the config. If these aliases are included, the
+  // tests will use the source code of the packages. If they are excluded,
+  // the packages will be resolved from the workspace and look for the `dist` folder.
+  const shouldTestDist = mode === "build";
+
+  const alias: Record<string, string> = {
+    "@auaust/pont-adapter-solid": path.resolve(__dirname, "src"),
+  };
+
+  if (!shouldTestDist) {
+    alias["@auaust/pont-core"] = path.resolve(__dirname, "../core/src");
+  }
+
+  return {
+    test: {
+      coverage: { provider: "v8" },
     },
-  },
+    resolve: {
+      alias,
+    },
+  };
 });
