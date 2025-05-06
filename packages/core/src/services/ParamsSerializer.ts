93a60c7bbb0329d729b824508d0d9d18c8353ee0
apademide <contact@apademide.ch>
2025-05-06 16:43:27 +0200

    cq: cleanup params serializer

--- packages/core/src/services/ParamsSerializer.ts
+++ packages/core/src/services/ParamsSerializer.ts
@@ -1,6 +1,5 @@
 import { A, B, N, O, P, S } from "@auaust/primitive-kit";
 import type { Pont } from "src/classes/Pont.js";
-import type { Primitive } from "src/types/utils.js";
 import { shouldAppend, type NormalizedUrlParams } from "src/utils/index.js";
 import type { Service } from "./index.js";
 
@@ -15,52 +14,13 @@ export interface ParamsSerializer extends Service {
   handle(pont: Pont, options: NormalizedUrlParams): URLSearchParams | string;
 }
 
-function getValue(
-  value: unknown
-): (Primitive | undefined[]) | Primitive | undefined;
-function getValue(value: unknown, preserveArrays: false): Primitive | undefined;
-function getValue(
-  value: unknown,
-  preserveArrays = true
-): (Primitive | undefined)[] | Primitive | undefined {
-  // null, undefined and NaN should be ignored
-  if (P.isNullish(value)) {
-    return undefined;
-  }
-
-  // Strings and numbers are returned as is, stringified,
-  // except for empty strings, which are ignored.
-  if (S.is(value) || N.is(value)) {
-    return S(value) || undefined;
-  }
-
-  // Booleans are returned as `"true"` or `"false"`.
-  if (B.is(value)) {
-    return B.toString(value);
-  }
-
-  // Objects are converted to JSON strings.
-  // If arrays are not preserved, they are also converted to JSON strings.
-  if (O.is(value, !preserveArrays)) {
-    return JSON.stringify(value);
-  }
-
-  // Arrays are preserved, but entries are converted.
-  if (A.is(value)) {
-    return value.map((v) => getValue(v, false));
-  }
-
-  // Any other value is stringified.
-  return S(value);
-}
-
-export function createDefaultParamsSerializer(pont: Pont): ParamsSerializer {
+export function createDefaultParamsSerializer(pont: Pont) {
   return {
-    handle: (pont, options) => {
+    handle(pont: Pont, options: NormalizedUrlParams) {
       const params = new URLSearchParams();
 
       for (const [key, value] of options) {
-        const serializedValue = getValue(value);
+        const serializedValue = this.valueKeepArrays(value);
 
         if (S.is(serializedValue)) {
           // If the key is bracketed, we want to append the value
@@ -89,6 +49,43 @@ export function createDefaultParamsSerializer(pont: Pont): ParamsSerializer {
       }
 
       return params;
+    },
+
+    valueKeepArrays(
+      value: unknown
+    ): (string | undefined)[] | string | undefined {
+      if (A.is(value)) {
+        return value.map((v) => this.value(v));
+      }
+
+      return this.value(value);
+    },
+
+    value(value: unknown): string | undefined {
+      // null, undefined and NaN should be ignored
+      if (P.isNullish(value)) {
+        return undefined;
+      }
+
+      // Strings and numbers are returned as is, stringified,
+      // except for empty strings, which are ignored.
+      if (S.is(value) || N.is(value)) {
+        return S(value) || undefined;
+      }
+
+      // Booleans are returned as `"true"` or `"false"`.
+      if (B.is(value)) {
+        return B.toString(value);
+      }
+
+      // Objects are converted to JSON strings.
+      // Arrays are also converted to JSON strings.
+      if (O.is(value, true)) {
+        return JSON.stringify(value);
+      }
+
+      // Any other value is stringified.
+      return S(value);
     },
   };
 }
