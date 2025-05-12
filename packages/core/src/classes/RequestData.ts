5b36bff121f25125fda6cab524c99952674e3f7d
apademide <contact@apademide.ch>
2025-05-12 21:58:55 +0200

    imp: include content-type in request headers

--- packages/core/src/classes/RequestData.ts
+++ packages/core/src/classes/RequestData.ts
@@ -1,3 +1,4 @@
+import type { WithPont } from "@core/src/classes/Pont.js";
 import type { Request } from "@core/src/classes/Request.js";
 import { hasBody } from "@core/src/utils/methods.js";
 import type Stream from "stream";
@@ -20,11 +21,15 @@ export type RequestDataInit =
   | undefined
   | null;
 
-export class RequestData {
+export class RequestData implements WithPont {
   protected readonly data: RequestDataInit;
 
   public constructor(protected request: Request, init?: RequestDataInit) {
     this.data = init ?? {};
+  }
+
+  public get pont() {
+    return this.request.pont;
   }
 
   public getContentType(): string {
--- packages/core/src/classes/RequestHeaders.ts
+++ packages/core/src/classes/RequestHeaders.ts
@@ -1,5 +1,6 @@
 import { S } from "@auaust/primitive-kit";
 import type { WithPont } from "@core/src/classes/Pont.js";
+import type { Request } from "@core/src/classes/Request.js";
 import { parseHeaders } from "@core/src/utils/headers.js";
 /**
  * A value that can be used to initialize the request headers.
@@ -9,22 +10,25 @@ export type RequestHeadersInit = HeadersInit;
 export class RequestHeaders implements WithPont {
   protected readonly headers: Record<string, string> = {};
 
-  public constructor(
-    protected readonly context: WithPont,
-    init?: RequestHeadersInit
-  ) {
+  public constructor(protected request: Request, init?: RequestHeadersInit) {
     this.headers = parseHeaders(init);
   }
 
   public get pont() {
-    return this.context.pont;
+    return this.request.pont;
   }
 
-  /**
-   * Returns the headers for the request, including the default headers from the header manager.
-   */
   public getHeaders(): Record<string, string> {
-    return this.pont.getHeadersManager().getHeaders(this.headers);
+    return this.pont.getHeadersManager().getHeaders({
+      ...this.getDefaultHeaders(),
+      ...this.headers,
+    });
+  }
+
+  public getDefaultHeaders(): Record<string, string> {
+    return {
+      "content-type": this.request.getContentType(),
+    };
   }
 
   public setHeader(key: string, value: string): this {
