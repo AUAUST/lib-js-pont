9c157101e47784b7623cb407bde6ae9054c20ce0
apademide <contact@apademide.ch>
2025-05-17 15:35:56 +0200

    fix: concerns mistakenly typing the base class as any

--- packages/core/src/concerns/Creatable.ts
+++ packages/core/src/concerns/Creatable.ts
@@ -2,7 +2,7 @@ import type { Constructor } from "@auaust/primitive-kit";
 import { concern } from "@core/src/concerns/index.js";
 
 export const Creatable = concern(
-  <B extends Constructor<any>>(base: B) =>
+  <B extends Constructor<object>>(base: B) =>
     class extends base {
       static create<T extends Constructor<any>>(
         this: T,
--- packages/core/src/concerns/HasSingleton.ts
+++ packages/core/src/concerns/HasSingleton.ts
@@ -2,11 +2,12 @@ import type { Constructor } from "@auaust/primitive-kit";
 import { concern } from "@core/src/concerns/index.js";
 
 export const HasSingleton = concern(
-  <B extends Constructor<any>>(base: B) =>
+  <B extends Constructor<object>>(base: B) =>
     class extends base {
       private static instance: InstanceType<B> | undefined;
 
       static getInstance(): InstanceType<B> {
+        // @ts-expect-error
         return (this.instance ??= new this());
       }
     }
