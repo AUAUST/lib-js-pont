0a56e1353fbd2ceb42652e0d22ffe05e5c16c533
apademide <contact@apademide.ch>
2025-05-19 13:30:34 +0200

    imp: more convenient enumValidator signature

--- packages/core/src/enums/index.ts
+++ packages/core/src/enums/index.ts
@@ -1,5 +1,12 @@
-export function enumValidator<T extends string>(enumObject: Record<string, T>) {
-  return (value: string): value is T => {
-    return Object.values(enumObject).includes(value as T);
+import { N, S } from "@auaust/primitive-kit";
+
+export function enumValidator<T extends string | number>(
+  enumObject: Record<string, T>
+) {
+  return (value: unknown): value is T => {
+    return (
+      (S.is(value) || N.is(value)) &&
+      Object.values(enumObject).includes(value as T)
+    );
   };
 }
